1416ac3a62e16d247bd73c791b32ce362e14367b
p_taghizade <p_taghizade@isc.co.ir>
2019-08-28 17:18:22 +0430

    init

---
+++ src/client/react/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react';
+import { Row, Col } from 'antd';
+
+import Header from './Header';
+import WelCome from './Welcome';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Row type="flex" justify="center">
+                <Col span={12}>
+                    <Header SelectedMenu="home" />
+                    <WelCome title="Welcome to my shop" />
+                    {/*<Products />
+                    <Footer />*/}
+                </Col>
+            </Row>
+        );
+    }
+}
+
---
+++ src/client/react/src/Welcome.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import { Row, Col} from 'antd';
+
+export default class WelCome extends React.Component {
+    render() {
+        return (
+            <Row>
+                <Col>
+                    <h1 className="header_title"> {this.props.title} </h1>
+                    <p className="header_description">description</p>
+                </Col>
+            </Row>
+        );
+    }
+}
