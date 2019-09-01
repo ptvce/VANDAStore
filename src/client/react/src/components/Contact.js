e6d5680787bf538f1abb62c23413c2c1f9d3ae35
unknown <ptvce@yahoo.com>
2019-09-01 22:39:05 +0430

    add file as db

---
+++ src/client/react/src/components/Contact.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'antd';
+
+import Header from './Header';
+import WelCome from './Welcome';
+
+
+
+export default class Contact extends React.Component {
+    render() {
+        return (
+            <Row type="flex" justify="center">
+                <Col span={12}>
+                    <WelCome title="Welcome to my Contact page" />
+                    <Header SelectedMenu="morepage:contact" />
+                </Col>
+            </Row>
+        );
+    }
+}
+
---
+++ src/client/react/src/components/Header.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Row, Col, Menu, Icon } from 'antd';
+import { Link } from 'react-router-dom';
+import 'antd/dist/antd.min.css';
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            current: this.props.SelectedMenu
+        }
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(e) {
+        // e.preventDefault() ;
+        this.setState({
+            current: e.key
+        });
+    }
+
+    render() {
+
+        return (
+
+            <Menu onClick={this.handleClick}
+                selectedKeys={[this.state.current]}
+                mode="horizontal">
+                <Menu.Item key="morepage:home">
+                    <Icon type="home" /> <Link style={{ display: 'inline-block' }} to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item key="app">
+                    <Icon type="appstore" />Products
+                </Menu.Item>
+                <Menu.Item key="alipay">
+                    <a href="https://roocket.ir" target="_blank">Example - Link</a>
+                </Menu.Item>
+                <SubMenu title={<span><Icon type="setting" />More Page</span>}>
+                    <MenuItemGroup title="Item 1">
+                        <Menu.Item key="morepage:about"><Link to="/about">About Page</Link></Menu.Item>
+                        <Menu.Item key="morepage:contact"><Link to="/contact">Contact Page</Link></Menu.Item>
+                    </MenuItemGroup>
+                </SubMenu>
+            </Menu>
+
+        );
+    }
+}
+
