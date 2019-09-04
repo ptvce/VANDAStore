c0f89155ad521e5a41c67d29c5029d49b76d01ed
p_taghizade <p_taghizade@isc.co.ir>
2019-09-04 08:29:44 +0430

    create list of products

--- src/client/react/src/App.js
+++ src/client/react/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
-//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import Data from './data.js';
 import Header from './components/Header';
 import WelCome from './components/Welcome';
+import Products from './components/Products';
 
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: Data,
+            orders: []
+        }
+        this.addToCard = this.addToCard.bind(this);
+    }
+
+    addToCard(key) {
+        if (this.state.products[key].available && this.state.orders.indexOf(key) == -1) {
+            this.state.orders.push(key);
+            this.setState((prevState) => {
+                orders: prevState.orders
+            });
+        }
+    }
+
     render() {
         return (
             <Row type="flex" justify="center">
                 <Col span={12}>
                     <Header SelectedMenu="home" />
                     <WelCome title="Welcome to my shop" />
+                    <Products products={this.state.products} addToCard={this.addToCard} />
                 </Col>
             </Row>
         );
---
+++ src/client/react/src/components/Products.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Col, Row } from 'antd';
+
+import h from './../helper';
+
+import Product from './Product';
+
+export default class Products extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.renderProduct = this.renderProduct.bind(this);
+        this.renderRow = this.renderRow.bind(this);
+        this.chunkProduct = h.array_chunk(Object.keys(this.props.products), 3);
+    }
+
+    renderProduct(key) {
+        console.log(key);
+        return (
+            <Col key={key} span={8}>
+                <Product details={this.props.products[key]} addToCard={this.props.addToCard} />
+            </Col>
+        );
+    }
+
+    renderRow(key) {
+        return (
+            <Row key={key}>
+                {this.chunkProduct[key].map(this.renderProduct)}
+            </Row>
+        );
+    }
+    render() {
+        console.log(this.chunkProduct);
+        return (
+            <Row>
+                <Col span={24}>
+                    <Row className="row-product">
+                        {Object.keys(this.chunkProduct).map(this.renderRow)}
+                    </Row>
+                </Col>
+            </Row>
+        );
+    }
+}
