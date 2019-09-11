b902011d551af2c695c5d3673986c513afd2bb6c
unknown <ptvce@yahoo.com>
2019-09-11 21:41:47 +0430

    nothing

--- src/client/react/src/index.js
+++ src/client/react/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router , Route  , browserRouter } from 'react-router-dom';
 
 import App from './App';
+import About from './components/About';
+import Contact from './components/Contact';
 
+const routes = (
+    <Router history={browserRouter}>
+      <Route path="/" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </Router>
+  );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routes, document.getElementById('root'));
