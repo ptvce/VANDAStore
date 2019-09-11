985845ec0c6095c2bd20eb8f629bdd5b38011346
unknown <ptvce@yahoo.com>
2019-09-11 22:00:55 +0430

    nothing

--- src/client/react/src/index.js
+++ src/client/react/src/index.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { Router , Route  , browserRouter } from 'react-router-dom';
-=======
 import { BrowserRouter as Router, Route, BrowserRouter, Switch } from 'react-router-dom';
->>>>>>> 9479e6868d288284cc4311f1e7f5512cf85a2ad8
 
 import App from './App';
 import About from './components/About';
 import Contact from './components/Contact';
-<<<<<<< HEAD
-=======
 import NotFind from './components/NotFind';
->>>>>>> 9479e6868d288284cc4311f1e7f5512cf85a2ad8
 
-const routes = (
-    <Router history={browserRouter}>
-      <Route path="/" component={App} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-    </Router>
-  );
 
-<<<<<<< HEAD
-=======
+
 const routes = (
     <BrowserRouter>
         <Router>
@@ -37,5 +22,4 @@ const routes = (
     </BrowserRouter>
 );
 
->>>>>>> 9479e6868d288284cc4311f1e7f5512cf85a2ad8
 ReactDOM.render(routes, document.getElementById('root'));
